fab932def8b5a0ddb8d9db15eadea63123578d2d
YaniLozanov <kronos1997@abv.bg>
2018-06-09 13:00:48 +0200

    First Test with video.

---
+++ Test Code/Test APP.01/Test APP.01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_APP._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
